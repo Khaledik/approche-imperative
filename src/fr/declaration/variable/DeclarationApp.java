7dce9916852a9f89509d4249e5f3cd34ca8048d9
Khaledik <khaled.ikhlef2014@gmail.com>
2024-10-02 11:37:53 +0200

    commit tp-02

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte myByte = 32;
+        short myShort = 1024;
+        int myInteger = 10;
+        long myLong = 30000000000L;
+        float myFloat = 12.4f;
+        double myDouble = 3.85412;
+        char myChar = 'K';
+        boolean myBoolean = true;
+        String myString = "I am a string";
+
+        System.out.println(myDouble);
+
+        String randomString = "Voici le résultat d'un calcul : \n 1+5=6";
+
+        System.out.println(randomString);
+
+    }
+}
